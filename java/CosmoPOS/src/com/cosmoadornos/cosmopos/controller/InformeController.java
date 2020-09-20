05418297cd652a65e0e4429fc13cc5968c96cbe2
CosmoPos <cosmoadornos@gmail.com>
2020-09-20 01:29:56 -0500

    class to manage the reports

---
+++ java/CosmoPOS/src/com/cosmoadornos/cosmopos/controller/InformeController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cosmoadornos.cosmopos.controller;
+
+/**
+ *
+ * @author Brayan Novoa <bnovoa.linux@gmail.com at cosmoadornos.com>
+ */
+public class InformeController {
+    
+}
